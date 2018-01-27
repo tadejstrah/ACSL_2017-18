4d8c1b6c1f18ee7606fe7be2a3aed75959a32574
tadejstrah <tadej.strah@gmail.com>
2018-01-27 16:49:32 +0100

    2

--- 2-kolo/2.py
+++ 2-kolo/2.py
@@ -1,8 +1,43 @@
-num_of_input_lines = 4
 x = []
+oklepaji = [None] * 4
+oklepaji_pos = [None] * 4
+manjkajoc = 0
+manjkajoc_pos = 0
 
-for i in range(num_of_input_lines):
-	x.append(input()) 
+with open("input") as y:
+	for neki in y:
+		x.append(neki)
 
-for neki in x:
-	print(neki)
+for line in x:
+	for i in range(4):
+		oklepaji[i] = False
+		oklepaji_pos[i] = 0
+
+
+	for i in range(len(line)):
+
+		if line[i] == "[":
+			oklepaji[0] = True
+			oklepaji_pos[0] = i+1
+		if line[i] == "]":
+			oklepaji[1] = True
+			oklepaji_pos[1] = i+1
+		if line[i] == "(":
+			oklepaji[2] = True	
+			oklepaji_pos[2] = i+1
+		if line[i] == ")":
+			oklepaji[3] = True
+			oklepaji_pos[3] = i+1
+
+	for i in range(len(oklepaji)):
+		if oklepaji[i] == False:
+			manjkajoc = i
+
+
+	print(line.strip())
+	#for i in range(len(oklepaji)):
+	#	print(oklepaji[i],oklepaji_pos[i])
+
+	print(manjkajoc)
+	#print(oklepaji,oklepaji_pos)
+	print()
